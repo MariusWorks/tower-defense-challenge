367ef4de67c8b86857dbbefa6058dc5edb17e273
MariusWorks <28069349+MariusWorks@users.noreply.github.com>
2024-07-01 11:08:20 +0200

    Initial creation of Unreal Engine Project

---
+++ Source/Tdc/Tdc.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tdc.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tdc, "Tdc" );
+
+DEFINE_LOG_CATEGORY(LogTdc)
+ 
---
+++ Source/Tdc/TdcGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TdcGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATdcGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATdcGameMode();
+};
+
+
+
